f76ff451418aa4d1575ca3fa818058e1fe68c40a
Peter Petushkov <58184233+Vaixle@users.noreply.github.com>
2023-10-26 16:45:17 +0400

    cors fix

--- src/main/java/com/vaixle/testapi/configuration/WebConfig.java
+++ src/main/java/com/vaixle/testapi/configuration/WebConfig.java
@@ -11,6 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
     }
 }
